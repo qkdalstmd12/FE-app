e8a708e0da0d0f89810e8e4cd535b419c1693ea0
yujin <yujin09219@naver.com>
2025-05-17 10:46:46 +0900

    [chore] close: #1 프로젝트 초기 세팅

---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const HomeScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>홈 화면입니다 🚀</Text>
+    </View>
+  );
+};
+
+export default HomeScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f2f2f2',
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+});
