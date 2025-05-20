e9de3c4ed3cc75583c48480290f020ab3058ef73
0000yuyu <156329445+0000yuyu@users.noreply.github.com>
2025-05-20 17:46:18 +0900

    fix: 파일 경로 변경
    Signed-off-by: 0000yuyu <156329445+0000yuyu@users.noreply.github.com>

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,6 @@
 import { Text, View } from 'react-native';
 
-export default function Home() {
+export default function Index() {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text>Runify</Text>
