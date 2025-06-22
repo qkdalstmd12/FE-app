bb1fbc697a2037e91160bf3a182e765a999b9870
yujin <yujin09219@naver.com>
2025-06-23 02:31:45 +0900

    refactor : 앱 진입화면 수정

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,20 @@
-import { Slot } from "expo-router";
+import { Stack } from 'expo-router';
+import { StyleSheet, View } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 export default function RootLayout() {
-  return <Slot />;
+  const insets = useSafeAreaInsets();
+
+  return (
+    <View style={[styles.container, { paddingTop: insets.top }]}>
+      <Stack screenOptions={{ headerShown: false }} />
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+});
