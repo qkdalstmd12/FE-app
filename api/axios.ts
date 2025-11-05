e512005ae13ec612b6be3b7e2907ee4fe48047fd
yujin <yujin09219@naver.com>
2025-11-05 13:06:35 +0900

    "feat : build Test"

--- api/axios.ts
+++ api/axios.ts
@@ -1,30 +1,23 @@
 import { getToken } from '@/utils/auth';
 import axios from 'axios';
-import Constants from 'expo-constants';
 
-let instance = null;
+const instance = axios.create({
+  baseURL: process.env.EXPO_PUBLIC_API_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
 
-if (Constants.expoConfig) {
-  // @ts-ignore
-  instance = axios.create({
-    baseURL: Constants.expoConfig.extra.apiUrl,
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  });
-  instance.interceptors.request.use(
-    async (config) => {
-      const token = await getToken();
-      console.log('axios', token);
-      if (token) {
-        config.headers.Authorization = `Bearer ${token}`;
-      }
-      return config;
-    },
-    (error) => Promise.reject(error),
-  );
-}
-
-if (instance == null) throw new Error('서버 주소가 존재하지 않습니다.');
+instance.interceptors.request.use(
+  async (config) => {
+    const token = await getToken();
+    console.log('axios', token);
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error),
+);
 
 export default instance;
