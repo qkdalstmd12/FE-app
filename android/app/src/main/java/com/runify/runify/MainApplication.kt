b2350049de6cb9f4da605a8815e65ae8168ed9d9
qkdalstmd12 <162536646+qkdalstmd12@users.noreply.github.com>
2025-11-15 17:59:30 +0900

    fix

--- android/app/src/main/java/com/runify/runify/MainApplication.kt
+++ android/app/src/main/java/com/runify/runify/MainApplication.kt
@@ -5,31 +5,36 @@ import android.content.Context
 import com.facebook.react.ReactApplication
 import com.facebook.react.ReactNativeHost
 import com.facebook.react.PackageList
+import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.soloader.SoLoader
 import expo.modules.ApplicationLifecycleDispatcher
 import expo.modules.ReactNativeHostWrapper
+import com.facebook.react.ReactPackage
 
 class MainApplication : Application(), ReactApplication {
 
-  private val mReactNativeHost: ReactNativeHost =
-    ReactNativeHostWrapper(this, object : ReactNativeHost(this) {
-      override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
-      override fun getPackages(): List<com.facebook.react.ReactPackage> =
-        PackageList(this).packages
-      override fun getJSMainModuleName(): String = "index"
-      override fun getApplication(): Application = this@MainApplication
-    })
+    private val reactNativeHost: ReactNativeHost =
+        ReactNativeHostWrapper(this, object : DefaultReactNativeHost(this) { 
+            override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
 
-  override fun getReactNativeHost(): ReactNativeHost = mReactNativeHost
+            override fun getPackages(): List<ReactPackage> {
+                // DefaultNewArchitectureEntryPoint.getPackages() 호출은 제외
+                return PackageList(this).packages 
+            }
 
-  override fun onCreate() {
-    super.onCreate()
-    SoLoader.init(this, false)
-    ApplicationLifecycleDispatcher.onApplicationCreate(this)
-  }
+            override fun getJSMainModuleName(): String = ".expo/.virtual-metro-entry" 
+        })
 
-  override fun attachBaseContext(base: Context) {
-    super.attachBaseContext(base)
-    ApplicationLifecycleDispatcher.onApplicationAttachBaseContext(this, base)
-  }
+    override fun getReactNativeHost(): ReactNativeHost = reactNativeHost
+
+    override fun onCreate() {
+        super.onCreate()
+        SoLoader.init(this, false)
+        ApplicationLifecycleDispatcher.onApplicationCreate(this)
+    }
+
+    override fun attachBaseContext(base: Context) {
+        super.attachBaseContext(base)
+        ApplicationLifecycleDispatcher.onApplicationAttachBaseContext(this, base)
+    }
 }
